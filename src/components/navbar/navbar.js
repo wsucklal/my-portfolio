e917d68fa022f7ca7b4a094a1aabd9123b624e3b
wsucklal <warrensucklal@gmail.com>
2023-10-29 16:46:28 -0400

    added navbar

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './navbar.scss';
+
+const Navbar = () => (
+  <div className="navbar" data-testid="Navbar">
+    Navbar Component
+  </div>
+);
+
+Navbar.propTypes = {};
+
+Navbar.defaultProps = {};
+
+export default Navbar;
