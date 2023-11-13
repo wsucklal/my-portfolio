0847dc02b8b700f9a05e1e75205f67b790ce35b6
wsucklal <warrensucklal@gmail.com>
2023-11-13 17:13:39 -0500

    aligned navbar for all media types

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -4,19 +4,19 @@ import './navbar.scss';
 
 const Navbar = () => (
   <div className="Navbar" data-testid="Navbar">
-      <div className="content">
-        Warren Sucklal
+      <div id="" className="nav-header nav-content">
+        <div className="nav-header-content">Warren Sucklal</div>
       </div>
-      <div className="content">
-        <div id="nav-btn-content" className="nav-btn">
+      <div id="" className="nav-btns-panel nav-content">
+        <div id="" className="nav-btn-content nav-btn">
           Who Am I?
         </div>
-        <div id="nav-btn-content" className="nav-btn">
+        <div id="" className="nav-btn-content nav-btn">
           My Experience
         </div>
-        <div id="nav-btn-content" className="nav-btn">
+        <div id="" className="nav-btn-content nav-btn">
           Contact
-          </div>
+        </div>
       </div>
   </div>
 );
