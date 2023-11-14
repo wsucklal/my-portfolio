984803e6f519219a6eabdf4258716bb7c832e02c
wsucklal <warrensucklal@gmail.com>
2023-11-13 23:37:59 -0500

    added contacts page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar/navbar';
-import Bio from './components/bio/bio'
+import Bio from './components/bio/bio';
+import Contact from './components/contact/contact';
+
 function App() {
   return (
     <div className="App">
       <div className="App-Content">
         <Navbar></Navbar>
         <Bio></Bio>
+        <Contact></Contact>
       </div>
     </div>
   );
---
+++ src/components/contact/contact.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './contact.scss';
+
+const Contact = () => (
+  <div className="Contact" data-testid="Contact">
+    <div className="contact-header contact-content">
+        <div className="contact-header-content">
+          Contacts
+        </div>
+    </div>
+    <div className="contact-link contact-content">
+      <div className="contact-link-content">
+        <div className=" contact-link-pic">
+          <img src="/assets/images/linkedInLogo.png"></img>
+        </div>
+        <div className="contact-link-text">
+          LinkedIn
+        </div>
+      </div>
+      <div className="contact-link-content">
+        <div className=" contact-link-pic">
+          <img src="/assets/images/Mail.png"></img>
+        </div>
+        <div className=" contact-link-text">
+          Send An Email
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+Contact.propTypes = {};
+
+Contact.defaultProps = {};
+
+export default Contact;
