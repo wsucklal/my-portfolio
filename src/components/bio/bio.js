8fd24bdd4c5c8cb48a2525ba3d8f0a6eb44a715b
wsucklal <warrensucklal@gmail.com>
2023-11-13 20:23:02 -0500

    adding in bio component

---
+++ src/components/bio/bio.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './bio.scss';
+
+const Bio = () => (
+  <div className="bio" data-testid="Bio">
+    Bio Component
+  </div>
+);
+
+Bio.propTypes = {};
+
+Bio.defaultProps = {};
+
+export default Bio;
