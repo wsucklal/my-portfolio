b45da798a278c325a153a4386b2bad52d00bd6c9
wsucklal <warrensucklal@gmail.com>
2023-11-28 11:39:46 -0500

    fixng hights

--- src/components/project/project.js
+++ src/components/project/project.js
@@ -3,8 +3,25 @@ import PropTypes from 'prop-types';
 import './project.scss';
 
 const Project = () => (
-  <div id="Project" className="project" data-testid="Project">
-    Project Component
+  <div id="Project" className="Project" data-testid="Project">
+    <div className='project-panel'>
+      <div className='project-panel-ctnt'>
+        <div className='project-display'>
+          Test
+        </div>
+        <div className='project-display-tech'>
+          Test
+        </div>
+      </div>
+      <div className='project-panel-ctnt'>
+        <div className='project-display-search'>
+            Test
+        </div>
+        <div className='project-display-list'>
+            Test
+        </div>
+      </div>
+    </div>
   </div>
 );
 
