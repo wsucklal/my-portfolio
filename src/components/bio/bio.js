0cdcbbfc234dc373a63006acb48befe083eaf413
wsucklal <warrensucklal@gmail.com>
2023-12-23 22:00:30 -0500

    added resume file download from bio

--- src/components/bio/bio.js
+++ src/components/bio/bio.js
@@ -2,6 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './bio.scss';
 
+
+
+
 const Bio = () => (
   <div id="Bio" className="Bio" data-testid="Bio">
     <div className="bio-pic bio-content">
@@ -13,14 +16,11 @@ const Bio = () => (
       <div className="bio-text-content">
         <p>Hi, I’m Warren! I am a young software engineer whose brain is always trying to solve problems thanks to a curious mind. I have been a logical, hands on, problem solver since I was about 5 years old starting with … Lego, sports, math,  science, and now my life long passion as a software engineer. Hope you enjoy the rest of my page and feel free to <a className='bio-text-reachout' href='#Contact'>reach out to me</a> if you have any questions. You can find my resume just below which has more details about my experiences. Thanks again!</p>
         <div className='bio-text-btn'>
-          {/* <a id="" className="bio-btn-content">
-             PDF - Resume
-          </a> */}
+          <a href="/assets/documents/Resume-Warren.pdf" download = "Resume-Warren.pdf" id="" className="bio-btn-content">
+            Resume - PDF 
+          </a>
         </div>
       </div>
-      {/* <div className="bio-text-content">
-        <p>Hi, I’m Warren! I am a young software engineer whose brain is always trying to solve problems thanks to a curious mind. I have been a logical, hands on, problem solver since I was about 5 years old starting with … Lego, sports, math,  science, and now my life long passion as a software engineer. Hope you enjoy the rest of my page and feel free to reach out to me if you have any questions. Thanks again!</p>
-      </div> */}
     </div>
   </div>
 );
